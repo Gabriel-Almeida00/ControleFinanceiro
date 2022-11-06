4fe486576038d4f3b8c1aee3b68666cc8fe7d885
gabriel <gaamdal557@gmail.com>
2022-11-06 12:21:29 -0300

    enviar formulario

--- ControleFinanceiro-UI/src/app/components/categoria/nova-categoria/nova-categoria.component.ts
+++ ControleFinanceiro-UI/src/app/components/categoria/nova-categoria/nova-categoria.component.ts
@@ -1,34 +1,46 @@
+import { CategoriasService } from './../../../services/categorias.service';
 import { TiposService } from './../../../services/tipos.service';
 import { Component, OnInit } from '@angular/core';
 import { Tipo } from 'src/app/models/Tipo';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nova-categoria',
   templateUrl: './nova-categoria.component.html',
-  styleUrls: ['./nova-categoria.component.css']
+  styleUrls: ['../listagem-categoria/listagem-categoria.component.css'],
 })
 export class NovaCategoriaComponent implements OnInit {
-
   formulario: any;
   tipos: Tipo[] | undefined;
 
-  constructor(private tipoService: TiposService) { }
+  constructor(
+    private tipoService: TiposService,
+    private categoriasService: CategoriasService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.tipoService.PegarTodos().subscribe(resultado => {
+    this.tipoService.PegarTodos().subscribe((resultado) => {
       this.tipos = resultado;
+      console.log(resultado);
     });
 
     this.formulario = new FormGroup({
-      nome : new FormControl(null),
-      icone : new FormControl(null),
-      tipoId : new FormControl(null),
+      nome: new FormControl(null),
+      icone: new FormControl(null),
+      tipoId: new FormControl(null),
     });
   }
-  
+
   get propriedade() {
     return this.formulario.controls;
   }
 
+  EnviarFormulario(): void {
+    const categoria = this.formulario.value;
+    this.categoriasService.NovaCategoria(categoria).subscribe((resultado) => {
+      this.router.navigate(['categorias/listagemcategorias']);
+    });
+  }
 }

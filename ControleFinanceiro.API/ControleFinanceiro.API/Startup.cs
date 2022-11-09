c1447fad95cebacb4dd9e1cfdbd4acbc7b0598d3
gabriel <gaamdal557@gmail.com>
2022-11-08 21:30:23 -0300

    funcao e registro

--- ControleFinanceiro.API/ControleFinanceiro.API/Startup.cs
+++ ControleFinanceiro.API/ControleFinanceiro.API/Startup.cs
@@ -1,4 +1,5 @@
 using ControleFinanceiro.API.Validacoes;
+using ControleFinanceiro.API.ViewModels;
 using ControleFinanceiro.DAL;
 using ControleFinanceiro.DAL.Interfaces;
 using ControleFinanceiro.DAL.Repositorios;
@@ -45,6 +46,8 @@ namespace ControleFinanceiro.API
 
 
             services.AddTransient<IValidator<Categoria>,CategoriaValidator>();
+            services.AddTransient<IValidator<FuncoesViewModel>, FuncoesViewModelValidator>();
+            services.AddTransient<IValidator<RegistroViewModel>, RegistroViewModelValidator>();
 
             services.AddCors();
 
